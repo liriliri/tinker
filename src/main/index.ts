f92d09a0ca1915cddafe372c3ad91e7e70e9b4b8
surunzi <redhoodsu@tencent.com>
2021-02-08 17:56:40 +0800

    chore: init

---
+++ src/main/index.ts
@@ -0,0 +1,55 @@
+import { app, BrowserWindow, Menu } from 'electron'
+import path from 'path'
+
+class App {
+  private win: BrowserWindow
+  constructor() {
+    this.win = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    })
+
+    this.init()
+  }
+  focus() {}
+  private init() {
+    this.initWin()
+  }
+  private initWin() {
+    const { win } = this
+
+    win.setMenu(
+      Menu.buildFromTemplate([
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'Toggle Developer Tools',
+              click() {
+                win.webContents.openDevTools()
+              },
+            },
+          ],
+        },
+      ])
+    )
+    win.loadFile(path.resolve('file://', __dirname, '../../index.html'))
+  }
+}
+
+const lock = app.requestSingleInstanceLock()
+
+if (!lock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    const app: App = (global as any).app
+    if (app) {
+      app.focus()
+    }
+  })
+  app.on('ready', () => {
+    ;(global as any).app = new App()
+  })
+}
