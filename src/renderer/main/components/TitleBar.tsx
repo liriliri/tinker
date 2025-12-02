b588b65d5f0df55a8b5ed2b8a93559d5542055c3
redhoodsu <redhoodsu@tencent.com>
2025-12-02 08:48:05 +0800

    chore: small changes

--- src/renderer/main/components/TitleBar.tsx
+++ src/renderer/main/components/TitleBar.tsx
@@ -2,5 +2,9 @@ import { observer } from 'mobx-react-lite'
 import Style from './Titlebar.module.scss'
 
 export default observer(function Titlebar() {
-  return <div className={Style.container}></div>
+  return <div className={Style.container}>
+    <div></div>
+    <input/>
+    <div></div>
+  </div>
 })
