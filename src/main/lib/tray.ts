3366f52a8732521506ba7f37c911da24f7e71bb9
redhoodsu <redhoodsu@tencent.com>
2025-12-06 10:23:17 +0800

    feat: theme and lang settings

--- src/main/lib/tray.ts
+++ src/main/lib/tray.ts
@@ -1,12 +1,22 @@
-import { app, Menu, nativeImage, Tray } from 'electron'
+import {
+  app,
+  Menu,
+  MenuItemConstructorOptions,
+  nativeImage,
+  Tray,
+} from 'electron'
 import { resolveResources } from 'share/main/lib/util'
 import * as main from '../window/main'
 import isMac from 'licia/isMac'
 import { t } from 'common/util'
+import { getSettingsStore } from './store'
 import log from 'share/common/log'
+import map from 'licia/map'
 
 const logger = log('tray')
 let tray: Tray | null = null
+
+const settingsStore = getSettingsStore()
 
 export function init() {
   const iconPath = isMac ? 'tray-template.png' : 'tray.png'
@@ -17,10 +27,12 @@ export function init() {
   tray = new Tray(icon)
   tray.setToolTip(`${PRODUCT_NAME} ${VERSION}`)
   tray.on('click', () => {
+    updateContextMenu()
     if (!isMac) {
       main.showWin()
     }
   })
+  tray.on('right-click', updateContextMenu)
 
   updateContextMenu()
 }
@@ -31,6 +43,45 @@ async function updateContextMenu() {
   }
 
   logger.info('update context menu')
+
+  const themeMenu: MenuItemConstructorOptions[] = map(
+    ['system', 'light', 'dark'],
+    (theme) => {
+      return {
+        label: t(theme === 'system' ? 'sysPreference' : theme),
+        type: 'radio',
+        checked: settingsStore.get('theme') === theme,
+        click() {
+          settingsStore.set('theme', theme)
+        },
+      }
+    }
+  )
+
+  const langMenu: MenuItemConstructorOptions[] = map(
+    ['system', 'en-US', 'zh-CN'],
+    (lang) => {
+      let label = t('sysPreference')
+      if (lang === 'en-US') {
+        label = 'English'
+      } else if (lang === 'zh-CN') {
+        label = '中文'
+      }
+
+      return {
+        label,
+        type: 'radio',
+        checked: settingsStore.get('language') === lang,
+        click() {
+          settingsStore.set('language', lang)
+          setTimeout(() => {
+            app.relaunch()
+            app.exit()
+          }, 500)
+        },
+      }
+    }
+  )
 
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -43,7 +94,25 @@ async function updateContextMenu() {
       type: 'separator',
     },
     {
-      label: t('quitTinker'),
+      label: t('theme'),
+      submenu: themeMenu,
+    },
+    {
+      label: t('language'),
+      submenu: langMenu,
+    },
+    {
+      type: 'separator',
+    },
+    {
+      label: t('restart'),
+      click() {
+        app.relaunch()
+        app.exit()
+      },
+    },
+    {
+      label: t('quit'),
       click() {
         app.quit()
       },
