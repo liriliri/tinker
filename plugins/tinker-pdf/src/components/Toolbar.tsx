d0e4d4026482bf43a00dcbafb2b05f6dbeeb7977
redhoodsu <redhoodsu@tencent.com>
2026-05-23 20:49:26 +0800

    chore: small changes

--- plugins/tinker-pdf/src/components/Toolbar.tsx
+++ plugins/tinker-pdf/src/components/Toolbar.tsx
@@ -16,6 +16,7 @@ import store from '../store'
 import { tw } from 'share/theme'
 import {
   Toolbar,
+  ToolbarSpacer,
   ToolbarSeparator,
   TOOLBAR_ICON_SIZE,
   ToolbarButton,
@@ -28,109 +29,99 @@ export default observer(function ToolbarComponent() {
 
   return (
     <>
-      <Toolbar>
+      <Toolbar className="relative">
         {/* Left side */}
-        <div className="flex items-center gap-0">
-          <ToolbarButton
-            onClick={() => store.toggleSidebar()}
-            title={t(store.sidebarOpen ? 'hideSidebar' : 'showSidebar')}
-            disabled={!store.pdfDoc}
-          >
-            {store.sidebarOpen ? (
-              <PanelLeftClose size={TOOLBAR_ICON_SIZE} />
-            ) : (
-              <PanelLeft size={TOOLBAR_ICON_SIZE} />
-            )}
-          </ToolbarButton>
+        <ToolbarButton
+          onClick={() => store.toggleSidebar()}
+          title={t(store.sidebarOpen ? 'hideSidebar' : 'showSidebar')}
+          disabled={!store.pdfDoc}
+        >
+          {store.sidebarOpen ? (
+            <PanelLeftClose size={TOOLBAR_ICON_SIZE} />
+          ) : (
+            <PanelLeft size={TOOLBAR_ICON_SIZE} />
+          )}
+        </ToolbarButton>
 
-          <ToolbarSeparator />
+        <ToolbarSeparator />
 
-          <ToolbarButton onClick={() => store.openFile()} title={t('openFile')}>
-            <FolderOpen size={TOOLBAR_ICON_SIZE} />
-          </ToolbarButton>
-        </div>
+        <ToolbarButton onClick={() => store.openFile()} title={t('openFile')}>
+          <FolderOpen size={TOOLBAR_ICON_SIZE} />
+        </ToolbarButton>
 
+        <ToolbarSpacer />
+
+        {/* Right side - export button */}
         {store.pdfDoc && (
-          <>
-            {/* Spacer to push controls to center */}
-            <div className="flex-1" />
+          <ToolbarButton
+            onClick={() => setExportDialogOpen(true)}
+            title={t('exportImages')}
+            disabled={!!store.exportProgress}
+          >
+            <Image size={TOOLBAR_ICON_SIZE} />
+          </ToolbarButton>
+        )}
 
-            {/* Center controls */}
-            <div className="flex items-center gap-2">
-              {/* Page navigation group - compact */}
-              <div className="flex items-center gap-0.5">
-                <ToolbarButton
-                  onClick={() => store.prevPage()}
-                  disabled={store.currentPage <= 1}
-                  title={t('previousPage')}
-                >
-                  <ChevronLeft size={TOOLBAR_ICON_SIZE} />
-                </ToolbarButton>
-
-                <span
-                  className={`text-xs ${tw.text.primary} min-w-[80px] text-center px-2`}
-                >
-                  {`${store.currentPage} / ${store.numPages}`}
-                </span>
-
-                <ToolbarButton
-                  onClick={() => store.nextPage()}
-                  disabled={store.currentPage >= store.numPages}
-                  title={t('nextPage')}
-                >
-                  <ChevronRight size={TOOLBAR_ICON_SIZE} />
-                </ToolbarButton>
-              </div>
-
-              <ToolbarSeparator />
-
-              {/* Zoom controls group - compact */}
-              <div className="flex items-center gap-0.5">
-                <ToolbarButton
-                  onClick={() => store.zoomOut()}
-                  disabled={store.scale <= 0.5}
-                  title={t('zoomOut')}
-                >
-                  <ZoomOut size={TOOLBAR_ICON_SIZE} />
-                </ToolbarButton>
-
-                <span
-                  className={`text-xs ${tw.text.primary} min-w-[40px] text-center px-1`}
-                >
-                  {`${Math.round(store.scale * 100)}%`}
-                </span>
-
-                <ToolbarButton
-                  onClick={() => store.zoomIn()}
-                  disabled={store.scale >= 3}
-                  title={t('zoomIn')}
-                >
-                  <ZoomIn size={TOOLBAR_ICON_SIZE} />
-                </ToolbarButton>
-
-                <ToolbarButton
-                  onClick={() => store.resetZoom()}
-                  title={t('resetZoom')}
-                >
-                  <RotateCcw size={TOOLBAR_ICON_SIZE} />
-                </ToolbarButton>
-              </div>
-            </div>
-
-            {/* Spacer to balance the layout */}
-            <div className="flex-1" />
-
-            {/* Right side - export button */}
-            <div className="flex items-center gap-0">
+        {/* Center controls - absolute positioned */}
+        {store.pdfDoc && (
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center gap-2">
+            <div className="flex items-center gap-0.5">
               <ToolbarButton
-                onClick={() => setExportDialogOpen(true)}
-                title={t('exportImages')}
-                disabled={!!store.exportProgress}
+                onClick={() => store.prevPage()}
+                disabled={store.currentPage <= 1}
+                title={t('previousPage')}
               >
-                <Image size={TOOLBAR_ICON_SIZE} />
+                <ChevronLeft size={TOOLBAR_ICON_SIZE} />
+              </ToolbarButton>
+
+              <span
+                className={`text-xs ${tw.text.primary} min-w-[80px] text-center px-2`}
+              >
+                {`${store.currentPage} / ${store.numPages}`}
+              </span>
+
+              <ToolbarButton
+                onClick={() => store.nextPage()}
+                disabled={store.currentPage >= store.numPages}
+                title={t('nextPage')}
+              >
+                <ChevronRight size={TOOLBAR_ICON_SIZE} />
               </ToolbarButton>
             </div>
-          </>
+
+            <ToolbarSeparator />
+
+            <div className="flex items-center gap-0.5">
+              <ToolbarButton
+                onClick={() => store.zoomOut()}
+                disabled={store.scale <= 0.5}
+                title={t('zoomOut')}
+              >
+                <ZoomOut size={TOOLBAR_ICON_SIZE} />
+              </ToolbarButton>
+
+              <span
+                className={`text-xs ${tw.text.primary} min-w-[40px] text-center px-1`}
+              >
+                {`${Math.round(store.scale * 100)}%`}
+              </span>
+
+              <ToolbarButton
+                onClick={() => store.zoomIn()}
+                disabled={store.scale >= 3}
+                title={t('zoomIn')}
+              >
+                <ZoomIn size={TOOLBAR_ICON_SIZE} />
+              </ToolbarButton>
+
+              <ToolbarButton
+                onClick={() => store.resetZoom()}
+                title={t('resetZoom')}
+              >
+                <RotateCcw size={TOOLBAR_ICON_SIZE} />
+              </ToolbarButton>
+            </div>
+          </div>
         )}
       </Toolbar>
 
