c904e58c5432801cf503425e3c30d2f792573a29
redhoodsu <redhoodsu@tencent.com>
2026-05-30 12:13:02 +0800

    chore: small changes

--- plugins/share/components/TextInput.tsx
+++ plugins/share/components/TextInput.tsx
@@ -8,7 +8,7 @@ export default function TextInput({
   type = 'text',
   ...rest
 }: TextInputProps) {
-  const baseClassName = `w-full px-3 py-2 border ${tw.border} ${tw.primary.focusBorder} rounded ${tw.bg.input} ${tw.text.primary} focus:outline-none focus:ring-1 ${tw.primary.focusRing}`
+  const baseClassName = `w-full px-2.5 py-1.5 text-sm border ${tw.border} ${tw.primary.focusBorder} rounded ${tw.bg.input} ${tw.text.primary} focus:outline-none focus:ring-1 ${tw.primary.focusRing}`
   const combinedClassName = `${baseClassName} ${className}`.trim()
 
   return <input type={type} className={combinedClassName} {...rest} />
