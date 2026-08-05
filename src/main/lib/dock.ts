4b54284258ced1894ef7c2947b2182f451842ec4
RedHoodSu <surunzi@foxmail.com>
2026-08-06 07:44:20 +0800

    fix: dock

--- src/main/lib/dock.ts
+++ src/main/lib/dock.ts
@@ -1,41 +1,33 @@
-import isMac from 'licia/isMac'
 import * as window from 'share/main/lib/window'
 import isEmpty from 'licia/isEmpty'
 import { app } from 'electron'
-import contain from 'licia/contain'
 
 export function hide() {
-  if (!isMac) {
+  if (!app.dock) {
     return
   }
 
   const visibleWins = window.getVisibleWins()
+  const mainWin = window.getWin('main')
   const onlyMain =
-    visibleWins.length === 1 && visibleWins[0] === window.getWin('main')
+    visibleWins.length === 1 && visibleWins[0] === mainWin
 
-  if (isEmpty(visibleWins)) {
+  if (isEmpty(visibleWins) || onlyMain) {
     app.dock.hide()
-  } else if (onlyMain) {
-    const win = window.getWin('main')
-    win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true })
-    win.focus()
-    win.setVisibleOnAllWorkspaces(false, {
-      visibleOnFullScreen: true,
-    })
+    if (onlyMain && mainWin) {
+      mainWin.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true })
+      mainWin.focus()
+      mainWin.setVisibleOnAllWorkspaces(false, {
+        visibleOnFullScreen: true,
+      })
+    }
   }
 }
 
 export function show() {
-  if (!isMac) {
+  if (!app.dock) {
     return
   }
 
-  setTimeout(() => {
-    const visibleWins = window.getVisibleWins()
-    const noMain = !contain(visibleWins, window.getWin('main'))
-
-    if (visibleWins.length > 1 || noMain) {
-      app.dock.show()
-    }
-  }, 500)
+  app.dock.show()
 }
