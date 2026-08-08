7d98be794cea661366f6134998413bc4874f9a2f
RedHoodSu <surunzi@foxmail.com>
2026-08-08 08:22:38 +0800

    fix: npm install

--- src/renderer/main/components/TitleBar.tsx
+++ src/renderer/main/components/TitleBar.tsx
@@ -168,7 +168,9 @@ export default observer(function Titlebar() {
         onDoubleClick={() => main.toggleWinMaximize()}
       >
         <input
-          className={`${Style.input}${store.plugin ? ` ${Style.withClose}` : ''}`}
+          className={`${Style.input}${
+            store.plugin ? ` ${Style.withClose}` : ''
+          }`}
           placeholder={t('searchTool')}
           autoFocus={true}
           value={store.filter}
