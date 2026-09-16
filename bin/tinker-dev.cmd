62b697cac0f68ec868021bda3acf029727c239d1
redhoodsu <redhoodsu@tencent.com>
2026-09-16 11:29:17 +0800

    fix: windows cli chinese output

--- bin/tinker-dev.cmd
+++ bin/tinker-dev.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+chcp 65001 >nul 2>&1
 set ELECTRON_RUN_AS_NODE=1
 "%~dp0..\node_modules\.bin\electron.cmd" "%~dp0..\dist\main\cli.js" %*
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
--- bin/win/tinker.cmd
+++ bin/win/tinker.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+chcp 65001 >nul 2>&1
 set ELECTRON_RUN_AS_NODE=1
 "%~dp0..\..\TINKER.exe" "%~dp0..\app.asar\main\cli.js" %*
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
